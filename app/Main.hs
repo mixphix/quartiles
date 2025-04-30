38588afd99ae0db77702aec90ac3c0da1db76064
Melanie Brown <brown.m@pm.me>
2025-04-30 15:10:42 -0400

    refactor

--- app/Main.hs
+++ app/Main.hs
@@ -6,13 +6,13 @@ import Data.Set qualified as Set
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.IO qualified as Text
-
-allCombs :: (Eq a) => [a] -> [[a]]
-allCombs xs =
-  [1 .. 4] >>= \n -> filter ((n ==) . length . List.nub) $ mapM (const xs) [1 .. n]
+import Data.Traversable (for)
 
 quartiles :: [Text] -> [Text]
-quartiles from = map fold $ allCombs from
+quartiles from = map fold do
+  n <- [1 .. 4]
+  filter ((n ==) . length . List.nub) do
+    for [1 .. n] (const from)
 
 main :: IO ()
 main = do
@@ -20,5 +20,5 @@ main = do
   qs <- Text.words <$> Text.getLine
   knownWords <- Set.fromList . Text.lines <$> Text.readFile "Top100000.txt"
   let results = List.sort $ filter (`Set.member` knownWords) (quartiles qs)
-  mapM_ Text.putStrLn results
   putStrLn $ show (length results) ++ " results"
+  mapM_ Text.putStrLn results
