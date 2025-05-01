c76db0163bd279d7605e79221f46459cb8bed3e2
Melanie Brown <brown.m@pm.me>
2025-05-01 12:15:20 -0400

    generalize tiles

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,14 @@ module Main where
 import Data.Foldable (Foldable (fold))
 import Data.List qualified as List
 import Data.Set qualified as Set
-import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.IO qualified as Text
 import Data.Traversable (for)
 
-quartiles :: [Text] -> [Text]
-quartiles from = map fold do
-  n <- [1 .. 4]
+-- length (tiles 3 [1 .. n]) == n^3 + n^2 + n + 1
+tiles :: (Eq a) => Int -> [a] -> [[a]]
+tiles m from = do
+  n <- [1 .. m]
   filter ((n ==) . length . List.nub) do
     for [1 .. n] (const from)
 
@@ -19,6 +19,6 @@ main = do
   putStrLn "Enter a space-separated list of quartiles:"
   qs <- Text.words <$> Text.getLine
   knownWords <- Set.fromAscList . Text.lines <$> Text.readFile "Top100000.txt"
-  let results = List.sort $ filter (`Set.member` knownWords) (quartiles qs)
+  let results = List.sort $ filter (`Set.member` knownWords) (map fold $ tiles 4 qs)
   putStrLn $ show (length results) ++ " results"
   mapM_ Text.putStrLn results
