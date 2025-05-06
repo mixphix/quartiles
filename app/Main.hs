b68b25e9de2e0d6fc33e9aa50bde5bdd481248ff
Melanie Brown <brown.m@pm.me>
2025-05-06 09:14:52 -0400

    improve time complexity of tiles

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,20 @@
 module Main where
 
-import Data.Foldable (Foldable (fold))
 import Data.List qualified as List
 import Data.Set qualified as Set
 import Data.Text qualified as Text
 import Data.Text.IO qualified as Text
 
--- length (tiles 3 [1 .. n]) == n^3 + n^2 + n + 1
-tiles :: (Eq a) => Int -> [a] -> [[a]]
+tiles :: (Ord a) => Int -> [a] -> [[a]]
 tiles m from = do
   n <- [1 .. m]
-  filter ((n ==) . length . List.nub) do
-    traverse (const from) [1 .. n]
+  filter ((n ==) . Set.size . Set.fromList) $ traverse (const from) [1 .. n]
 
 main :: IO ()
 main = do
   putStrLn "Enter a space-separated list of quartiles:"
   qs <- Text.words <$> Text.getLine
   knownWords <- Set.fromDistinctAscList . Text.lines <$> Text.readFile "words.txt"
-  let results = List.sort $ filter (`Set.member` knownWords) (map fold $ tiles 4 qs)
+  let results = List.sort $ filter (`Set.member` knownWords) (Text.concat <$> tiles 4 qs)
   putStrLn $ show (length results) ++ " results"
   mapM_ Text.putStrLn results
